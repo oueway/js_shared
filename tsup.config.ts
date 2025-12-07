d1a22f7d2557240d3d5db870f88e67f2f1422ea2
oueway <oueway.forest@gmail.com>
2025-12-06 20:04:04 -0500

    1.0.1

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts', 'src/lib/index.ts', 'src/components/index.ts', 'src/pages/index.ts'],
   format: ['esm'],
   dts: true,
-  splitting: false,
+  splitting: true,
   sourcemap: true,
   clean: true,
   external: ['react', 'react-dom', 'next', '@supabase/ssr', '@supabase/supabase-js', 'lucide-react'],
